3cdcb531be0a59e8803e2f4217c30b29da3607e9
LENOVO-PC <rafhaelgigalucas@gmail.com>
2023-10-20 08:14:04 -0300

    penultila tela

--- src/components/data-card/index.tsx
+++ src/components/data-card/index.tsx
@@ -1,145 +1,240 @@
-import { Box, Typography, FormControl, OutlinedInput, InputAdornment } from "@mui/material";
+import {
+  Box,
+  FormControl,
+  InputAdornment,
+  OutlinedInput,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
 import { ICalcData } from "../../interfaces";
 
-export function DataCard({ 
-        label,
-        borderColor,
-        isPrejudice=false,
-        backgroundColor='#EAECF0',
-        costTax=false,
-        data,
-        fixedInad,
-        posMatricula=false
-    }: { 
-        label: string 
-        borderColor?: string
-        isPrejudice?: boolean
-        backgroundColor?: string
-        costTax?: boolean
-        data: ICalcData
-        fixedInad?: number
-        posMatricula?: boolean
-    }) {
-    const fontColor = backgroundColor === '#EAECF0' ? 'black' : 'white';
-    const [inadPercent, setInadPercent] = useState(10)
-    
-    const displayCurrency = (total: number) => {
-        return total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
-    }
+export function DataCard({
+  label,
+  borderColor,
+  isPrejudice = false,
+  backgroundColor = "#EAECF0",
+  costTax = false,
+  data,
+  fixedInad,
+  posMatricula = false,
+}: {
+  label: string;
+  borderColor?: string;
+  isPrejudice?: boolean;
+  backgroundColor?: string;
+  costTax?: boolean;
+  data: ICalcData;
+  fixedInad?: number;
+  posMatricula?: boolean;
+}) {
+  const fontColor = backgroundColor === "#EAECF0" ? "black" : "white";
+  const [inadPercent, setInadPercent] = useState(10);
 
-    return(
-        <>
-            <Box sx={{ 
-                display: 'flex', 
-                flexDirection: 'column', 
-                alignItems: 'center', 
-                backgroundColor: backgroundColor, 
-                padding: '40px',
-                borderRadius: '15px',
-                borderColor: borderColor ? borderColor : null,
-                borderWidth: borderColor ? '2px' : null,
-                borderStyle: borderColor ? 'solid' : null,
-                gap: '21px'
-            }}>
-                <Typography sx={{ fontSize: '24px', fontWeight: '600', color: fontColor }}>{label}</Typography>
-                <Box>
-                    <Typography sx={{ fontSize: '16px', color: fontColor, fontWeight: '600', textAlign: 'start' }}>Receita prevista</Typography>
-                    <FormControl variant="outlined">
-                        <OutlinedInput
-                            id="outlined-adornment-weight"
-                            aria-describedby="outlined-weight-helper-text"
-                            inputProps={{
-                                'aria-label': 'weight',
-                                }}
-                            value={displayCurrency(parseFloat(data.alunos) * parseFloat(data.ticketMedio) * 12)}
-                            disabled
-                            sx={{ 
-                                minWidth: '400px', 
-                                "& .MuiInputBase-input.Mui-disabled": {
-                                    background: "white",
-                                    color: "black !important",
-                                    WebkitTextFillColor: "black"
-                                } 
-                            }}
-                        />
-                    </FormControl>
-                </Box>
-                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'start', width: '100%' }}>
-                    <Typography sx={{ fontSize: '16px', color: fontColor, fontWeight: '600', textAlign: 'start' }}>{costTax ? '% Taxa de custo' : '% Inadimplência'}</Typography>
-                    <FormControl variant="outlined" sx={{ width: '100%' }}>
-                        <OutlinedInput
-                            id="outlined-adornment-weight"
-                            type="number"
-                            aria-describedby="outlined-weight-helper-text"
-                            endAdornment={<InputAdornment position="end">%</InputAdornment>}
-                            inputProps={{
-                                'aria-label': 'weight',
-                                }}
-                            value={fixedInad ? fixedInad : inadPercent}
-                            onChange={(event) => setInadPercent(parseFloat(event.target.value))}
-                            disabled={!costTax}
-                            sx={{ backgroundColor: 'white' }}
-                            // sx={{
-                            //     backgroundColor: 'white',
-                            //     "& .MuiInputBase-input.Mui-disabled": {
-                            //         background: "white",
-                            //         WebkitTextFillColor: "black"
-                            //     }
-                            // }}
-                        />
-                    </FormControl>
-                </Box>
-                <Box>
-                    <Typography sx={{ fontSize: '16px', color: fontColor, fontWeight: '600', textAlign: 'start' }}>Prejuízo {posMatricula ? 'Pós-matricula' : null}</Typography>
-                    <FormControl variant="outlined" error={isPrejudice}>
-                        <OutlinedInput
-                            id="outlined-adornment-weight"
-                            aria-describedby="outlined-weight-helper-text"
-                            inputProps={{
-                                'aria-label': 'weight',
-                                }}
-                            value={displayCurrency((parseFloat(data.alunos) * parseFloat(data.ticketMedio) * 12) * ((fixedInad ? fixedInad : inadPercent)/100))}
-                            disabled
-                            sx={{ 
-                                minWidth: '400px', 
-                                "& .MuiInputBase-input.Mui-disabled": {
-                                    background: "white",
-                                    WebkitTextFillColor: "black"
-                                },
-                                borderStyle: isPrejudice ? 'none none solid none' : null,
-                                borderWidth: '2px',
-                                borderColor: '#FF0000'
-                            }}
-                        />
-                    </FormControl>
-                </Box>
-                <Box>
-                    <Typography sx={{ fontSize: '16px', color: fontColor, fontWeight: '600', textAlign: 'start' }}>Receita Recebida</Typography>
-                    <FormControl variant="outlined" error={isPrejudice}>
-                        <OutlinedInput
-                            id="outlined-adornment-weight"
-                            aria-describedby="outlined-weight-helper-text"
-                            inputProps={{
-                                'aria-label': 'weight',
-                                }}
-                                value={displayCurrency((parseFloat(data.alunos) * parseFloat(data.ticketMedio) * 12) * (1 - (fixedInad ? fixedInad : inadPercent)/100))}
-                            disabled
-                            sx={{ 
-                                minWidth: '400px', 
-                                "& .MuiInputBase-input.Mui-disabled": {
-                                    background: "white",
-                                    color: "black !important",
-                                    WebkitTextFillColor: "black"
-                                },
-                                borderStyle: isPrejudice ? 'none none solid none' : null,
-                                borderWidth: '2px',
-                                borderColor: '#FF0000'
-                            }}
-                        />
-                    </FormControl>
-                </Box>
-            </Box>
-        </>
-    )
+  const displayCurrency = (total: number) => {
+    return total.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+  };
+
+  return (
+    <>
+      <Box
+        sx={(theme) => ({
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          backgroundColor: backgroundColor,
+          borderRadius: "15px",
+          borderColor: borderColor ? borderColor : null,
+          borderWidth: borderColor ? "2px" : null,
+          borderStyle: borderColor ? "solid" : null,
+          gap: "21px",
+          padding: "40px",
+          [theme.breakpoints.down("lg")]: {
+            gap: "10px",
+            padding: "18px",
+          },
+        })}
+      >
+        <Typography
+          sx={{
+            fontSize: "24px",
+            fontWeight: "600",
+            color: fontColor,
+          }}
+        >
+          {label}
+        </Typography>
+        <Box>
+          <Typography
+            sx={(theme) => ({
+              fontSize: "16px",
+              [theme.breakpoints.down("lg")]: {
+                fontSize: "16px",
+              },
+              color: fontColor,
+              fontWeight: "600",
+              textAlign: "start",
+            })}
+          >
+            Receita prevista
+          </Typography>
+          <FormControl variant="outlined">
+            <OutlinedInput
+              id="outlined-adornment-weight"
+              aria-describedby="outlined-weight-helper-text"
+              inputProps={{
+                "aria-label": "weight",
+              }}
+              value={displayCurrency(
+                parseFloat(data.alunos) * parseFloat(data.ticketMedio) * 12
+              )}
+              disabled
+              sx={(theme) => ({
+                [theme.breakpoints.down("lg")]: {
+                  minWidth: "150px",
+                  width: "100%",
+                },
+                minWidth: "400px",
+                "& .MuiInputBase-input.Mui-disabled": {
+                  background: "white",
+                  color: "black !important",
+                  WebkitTextFillColor: "black",
+                },
+              })}
+            />
+          </FormControl>
+        </Box>
+        <Box>
+          <Typography
+            sx={{
+              fontSize: "16px",
+              color: fontColor,
+              fontWeight: "600",
+              textAlign: "start",
+            }}
+          >
+            {costTax ? "% Taxa de custo" : "% Inadimplência"}
+          </Typography>
+          <FormControl variant="outlined" sx={{ width: "100%" }}>
+            <OutlinedInput
+              id="outlined-adornment-weight"
+              type="number"
+              aria-describedby="outlined-weight-helper-text"
+              endAdornment={<InputAdornment position="end">%</InputAdornment>}
+              inputProps={{
+                "aria-label": "weight",
+              }}
+              value={fixedInad ? fixedInad : inadPercent}
+              onChange={(event) =>
+                setInadPercent(parseFloat(event.target.value))
+              }
+              disabled={!costTax}
+              sx={(theme) => ({
+                backgroundColor: "white",
+                minWidth: 400,
+                [theme.breakpoints.down("lg")]: {
+                  width: "100%",
+                  minWidth: 150,
+                },
+              })}
+              // sx={{
+              //     backgroundColor: 'white',
+              //     "& .MuiInputBase-input.Mui-disabled": {
+              //         background: "white",
+              //         WebkitTextFillColor: "black"
+              //     }
+              // }}
+            />
+          </FormControl>
+        </Box>
+        <Box>
+          <Typography
+            sx={{
+              fontSize: "16px",
+              color: fontColor,
+              fontWeight: "600",
+              textAlign: "start",
+            }}
+          >
+            Prejuízo {posMatricula ? "Pós-matricula" : null}
+          </Typography>
+          <FormControl variant="outlined" error={isPrejudice}>
+            <OutlinedInput
+              id="outlined-adornment-weight"
+              aria-describedby="outlined-weight-helper-text"
+              inputProps={{
+                "aria-label": "weight",
+              }}
+              value={displayCurrency(
+                parseFloat(data.alunos) *
+                  parseFloat(data.ticketMedio) *
+                  12 *
+                  ((fixedInad ? fixedInad : inadPercent) / 100)
+              )}
+              disabled
+              sx={(theme) => ({
+                minWidth: "400px",
+                [theme.breakpoints.down("lg")]: {
+                  width: "100%",
+                  minWidth: 150,
+                },
+                "& .MuiInputBase-input.Mui-disabled": {
+                  background: "white",
+                  WebkitTextFillColor: "black",
+                },
+                borderStyle: isPrejudice ? "none none solid none" : null,
+                borderWidth: "2px",
+                borderColor: "#FF0000",
+              })}
+            />
+          </FormControl>
+        </Box>
+        <Box>
+          <Typography
+            sx={{
+              fontSize: "16px",
+              color: fontColor,
+              fontWeight: "600",
+              textAlign: "start",
+            }}
+          >
+            Receita Recebida
+          </Typography>
+          <FormControl variant="outlined" error={isPrejudice}>
+            <OutlinedInput
+              id="outlined-adornment-weight"
+              aria-describedby="outlined-weight-helper-text"
+              inputProps={{
+                "aria-label": "weight",
+              }}
+              value={displayCurrency(
+                parseFloat(data.alunos) *
+                  parseFloat(data.ticketMedio) *
+                  12 *
+                  (1 - (fixedInad ? fixedInad : inadPercent) / 100)
+              )}
+              disabled
+              sx={(theme) => ({
+                minWidth: "400px",
+                [theme.breakpoints.down("lg")]: {
+                  width: "100%",
+                  minWidth: 150,
+                },
+                "& .MuiInputBase-input.Mui-disabled": {
+                  background: "white",
+                  color: "black !important",
+                  WebkitTextFillColor: "black",
+                },
+                borderStyle: isPrejudice ? "none none solid none" : null,
+                borderWidth: "2px",
+                borderColor: "#FF0000",
+              })}
+            />
+          </FormControl>
+        </Box>
+      </Box>
+    </>
+  );
 }
--- src/components/result/index.tsx
+++ src/components/result/index.tsx
@@ -1,61 +1,124 @@
-import { Box, Button } from "@mui/material";
-import { Typography } from '@mui/material';
+import { Box, Button, Typography } from "@mui/material";
 import { ICalcData } from "../../interfaces";
 import { DataCard } from "../data-card";
 
-export function Result({ data, onChangePage, onBack }: { data: ICalcData, onChangePage: () => any, onBack: () => any }) {
-    return(
-        <>
-            <Box sx={{ 
-                display: 'flex',
-                flexDirection: 'column', 
-                width: '100vw', 
-                height: '100vh', 
-                backgroundColor: '#fff',
-                alignItems: 'center'
-            }}>
-                <Typography sx={{ 
-                    fontSize: '60px', 
-                    fontWeight: '500', 
-                    color: 'black', 
-                    padding: '30px',
-                    textAlign: 'center'
-                }}>
-                    Olha só o quanto você pode estar perdendo...
-                </Typography>
-                <Box sx={{ display: 'flex', gap: '40px' }}>
-                    <DataCard data={data} label="Atualmente" borderColor="#FF0000" isPrejudice fixedInad={parseFloat(data.inadMedia)} />
-                    <DataCard data={data} label='Com "inadimplência 0"' costTax isPrejudice />
-                    <DataCard data={data} label="Com a VOZ" backgroundColor="#FF8424" fixedInad={5} />
-                </Box>
-                <Box sx={{ display: 'flex', marginTop: '60px', gap: '15px', alignItems: 'center' }}>
-                    <Button 
-                        sx={{  
-                            fontSize: '16px',
-                            fontWeight: '500',
-                            padding: '15px',
-                            maxHeight: '60%'
-                        }} 
-                        onClick={onBack} 
-                        variant="outlined"
-                    >
-                        Voltar
-                    </Button>
-                    <Button 
-                        sx={{ 
-                            fontSize: '24px',
-                            fontWeight: '500',
-                            minWidth: '500px',
-                            padding: '20px',
-                            backgroundColor: '#FF8424'
-                        }} 
-                        onClick={onChangePage} 
-                        variant="contained"
-                    >
-                        CONTINUAR
-                    </Button>
-                </Box>                
-            </Box>
-        </>
-    )
+export function Result({
+  data,
+  onChangePage,
+  onBack,
+}: {
+  data: ICalcData;
+  onChangePage: () => any;
+  onBack: () => any;
+}) {
+  return (
+    <>
+      <Box
+        sx={(theme) => ({
+          display: "flex",
+          flexDirection: "column",
+          width: "100vw",
+          height: "100vh",
+          backgroundColor: "#fff",
+          alignItems: "center",
+          [theme.breakpoints.down("lg")]: {
+            overflowY: "scroll",
+            paddingTop: "30px",
+          },
+        })}
+      >
+        <Typography
+          sx={(theme) => ({
+            fontSize: "60px",
+            [theme.breakpoints.down("lg")]: {
+              fontSize: "20px",
+            },
+            fontWeight: "500",
+            color: "black",
+            padding: "30px",
+            textAlign: "center",
+          })}
+        >
+          Olha só o quanto você pode estar perdendo...
+        </Typography>
+        <Box
+          sx={(theme) => ({
+            display: "flex",
+            gap: "40px",
+            [theme.breakpoints.down("lg")]: {
+              flexDirection: "column",
+            },
+          })}
+        >
+          <DataCard
+            data={data}
+            label="Atualmente"
+            borderColor="#FF0000"
+            isPrejudice
+            fixedInad={parseFloat(data.inadMedia)}
+          />
+          <DataCard
+            data={data}
+            label='Com "inadimplência 0"'
+            costTax
+            isPrejudice
+          />
+          <DataCard
+            data={data}
+            label="Com a VOZ"
+            backgroundColor="#FF8424"
+            fixedInad={5}
+          />
+        </Box>
+        <Box
+          sx={(theme) => ({
+            display: "flex",
+            marginTop: "60px",
+            marginBottom: "50px",
+            gap: "15px",
+            [theme.breakpoints.down("lg")]: {
+              flexDirection: "row",
+            },
+            alignItems: "center",
+          })}
+        >
+          <Button
+            sx={(theme) => ({
+              fontSize: "16px",
+              fontWeight: "500",
+              padding: "15px",
+              maxHeight: "60%",
+              [theme.breakpoints.down("lg")]: {
+                flexDirection: "column",
+                padding: "5px",
+              },
+            })}
+            onClick={onBack}
+            variant="outlined"
+          >
+            Voltar
+          </Button>
+          <Button
+            sx={(theme) => ({
+              fontSize: "24px",
+              fontWeight: "500",
+              minWidth: "500px",
+              padding: "20px",
+              backgroundColor: "#FF8424",
+              [theme.breakpoints.down("lg")]: {
+                flexDirection: "column",
+                fontSize: "18px",
+                minWidth: "200px",
+                padding: "5px",
+              },
+            })}
+            onClick={onChangePage}
+            variant="contained"
+          >
+            CONTINUAR
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
 }
